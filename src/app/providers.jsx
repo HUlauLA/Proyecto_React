d2e3e26045015148da860ff76639250e574e57c8
René Guevara <renealfaro753@gmail.com>
2025-09-23 23:54:47 -0600

    Usuario y Perfil

--- src/app/providers.jsx
+++ src/app/providers.jsx
@@ -1,8 +1,11 @@
 "use client";
 import { AuthProvider } from "@/context/AuthContext"; 
+import { UserProvider } from "@/context/UserContext";
 
 export default function Providers({ children }) {
   return <AuthProvider>
+            <UserProvider>
             {children}
+            </UserProvider>
         </AuthProvider>;
 }
